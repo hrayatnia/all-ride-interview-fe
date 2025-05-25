e9b476f06bbcdb7de0ff8f9a9d60ea58f525f802
Sam Rayatnia <h.rayatnia@gmail.com>
2025-05-25 18:15:33 +0200

    Removes unused AppRoutes import
    Cleans up the index.tsx file by removing an unused import statement for AppRoutes.
    
    This simplifies the code and removes a potential source of confusion.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App, { AppRoutes } from './App';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
